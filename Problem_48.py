8abf4ef4a81d82db72b8d8de385748f2fb741d61
tolstiy_07 <tolstiy07@gmail.con>
2024-08-16 12:54:10 +0300

    commit 2024_08_16

--- Problem_48.py
+++ Problem_48.py
@@ -6,3 +6,17 @@
 Найдите последние десять цифр суммы 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 
 '''
+from time import time
+
+def main():
+    start = time()
+
+    sum_num = 0
+    for x in range(1,1000):
+        sum_num += (x**x)
+    print(f""".......{(str(sum_num))[-10:]} - последние десять цифр суммы 1^1 + 2^2 + 3^3 + ... + 1000^1000.""")
+
+    print(f"Program running time {round(time() - start, 2)} sec.")
+
+if __name__ == "__main__":
+    main()
